ef28ab8d35574e2e6dbc267e98300e16995971b4
samiac2106 <samiac2106@gmail.com>
2020-10-03 19:05:00 -0500

    formula

--- salario.php
+++ salario.php
@@ -1,97 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Salarios</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Postobon</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
+
 <body>
-<header>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">SupermercadoJ1</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-     
-     
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-</header>
-
-<main>
-<div class="container">
-    <div class="row justify-content-center">
+  <main>
+    <div class="container">
+      <div class="row justify-content-center">
         <div class="col-4">
-        <form class="mt-5" action="index.php" method= "POST">
-        <h4 class="text-center">  ORDEN DE COMPRA </h4>
-        <div class="row">
-        <div class="col">
-        <input type="text" class="form-control" placeholder="Producto1" name="producto1">
-        </div>
-            <div class="col">
-            <input type="number" class="form-control" placeholder="Precio" name="precio1">
+          <form class="mt-5" action="" method="POST">
+            <h4 class="text-center"> SALARIO </h4>
+            <div class="row">
+              <div class="col">
+                <input type="number" class="form-control" placeholder="HORAS LABORADAS" name="horas_laboradas">
+              </div>          
             </div>
-            </div>
-        <button type="submit" class="btn btn-primary mt-5" name="botonEnvio">Submit</button> 
-        </form>
-
-        <?php if(isset($_POST["botonEnvio"])){
-              $productoA=$_POST["producto1"];
-              $precioA=$_POST["precio1"];
-              $costoEnvio=8000;
-              $total=$precioA+$costoEnvio;
-              echo("El total es de:   ".$total);  
-              
-
-              $numeros=array (1, 2,3);
-
-              echo ("<br>");
-              $arreglo=array ();
-              $sumatoria=0;
-
-             
-              foreach ($numeros as $valor)
-              {
-                  $sumatoria=$sumatoria+$valor)
-
-               }
-            echo ("<br>");
-            echo ($sumatoria);
-            
-            if($sumatoria>30 && $sumatoria>40{
-                echo("la sucursal A tiene mejor salario ")
-            }
- 
-             ?>
+            <button type="submit" class="btn btn-primary mt-5" name="botonEnvio">Submit</button>
+          </form>
+          <?php if (isset($_POST["botonEnvio"])) {
+            $cantHoras = $_POST["horas_laboradas"];
+            $horaSemana = $cantHoras - 40;
+            $horaExtra= 25000;
+            $salarioBas= 40*20000;
+            $result = '';
         
-                    
-      
+          if ($cantHoras <= 40) {
+            $result = $cantHoras * 20000;
+            echo ("El total a pagar es de:   " . $result);
+          } elseif ($cantHoras > 40) {
+            $result = $horaSemana * 25000 + $salarioBas;
+            echo ("El total a pagar es de:   " . $result);
+          } 
+        }
+          ?>
+
 
         </div>
+      </div>
     </div>
-</div>
-</main>
+  </main>
 
-<footer>
+  <footer>
 
-</footer>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
+
 </html>
